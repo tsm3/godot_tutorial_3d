fd8faf70057a7665f2c9bac5672906d7c14877bc
Rosemary <tristan.slayde.mansfield@gmail.com>
2024-01-21 15:19:20 -0600

    First and Last

---
+++ Ground.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+
+@export var groundSize3 = Vector3(60,2,60)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
